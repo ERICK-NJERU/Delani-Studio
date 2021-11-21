b001deffd087e78cdca4a2b4a36fc324906132d9
erick njeru <erick.njeru@student.moringaschool.com>
2021-11-21 14:30:40 +0300

    fixed what we do section

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   $(".design").click(function() {
     $(".design1").toggle("slow");
   });
-  $(".developments").click(function() {
+  $(".development").click(function() {
     $(".design2").toggle("slow");
   });
-  $(".productsm").click(function() {
+  $(".management").click(function() {
     $(".design3").toggle("slow");
   });
   $(".what").css("text-align", "center");
