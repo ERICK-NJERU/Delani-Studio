4c6340064321b8485d938f00176648ff0d2eecda
erick njeru <erick.njeru@student.moringaschool.com>
2021-11-21 11:49:57 +0300

    fixed js/ jsbootstrap

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $(".design").click(function() {
+    $(".dezine").toggle("slow");
+  });
+  $(".development").click(function() {
+    $(".developments").toggle("slow");
+  });
+  $(".management").click(function() {
+    $(".productsm").toggle("slow");
+  });
+  $(".what").css("text-align", "center");
+});
